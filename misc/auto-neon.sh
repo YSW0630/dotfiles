36da70107d0ca4029c834b167ae6cd211cafd1ae
pacfrog <justin.sw.yang@gmail.com>
2025-12-09 11:48:58 +0800

    Change neon wallpapers name

--- misc/auto-neon.sh
+++ misc/auto-neon.sh
@@ -11,13 +11,26 @@ export DBUS_SESSION_BUS_ADDRESS="${DBUS_SESSION_BUS_ADDRESS:-unix:path=/run/user
 
 # Neon-wallpapers
 WALLPAPERS=(
-  "$HOME/.local/share/backgrounds/2025-08-25-15-11-52-neon-01.jpg"
-  "$HOME/.local/share/backgrounds/2025-08-25-15-11-52-neon-02.jpg"
-  "$HOME/.local/share/backgrounds/2025-08-25-15-11-52-neon-03.jpg"
-  "$HOME/.local/share/backgrounds/2025-08-25-15-11-52-neon-04.jpg"
-  "$HOME/.local/share/backgrounds/2025-08-25-15-11-52-neon-05.jpg"
-  "$HOME/.local/share/backgrounds/2025-08-25-15-11-52-neon-06.jpg"
-  "$HOME/.local/share/backgrounds/2025-08-25-15-11-52-neon-07.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon10.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon11.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon12.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon13.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon14.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon15.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon16.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon17.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon18.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon19.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon1.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon20.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon2.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon3.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon4.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon5.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon6.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon7.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon8.jpg"
+	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon9.jpg"
 )
 
 # Read/Write the value in wall_index
