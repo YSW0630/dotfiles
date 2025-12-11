66b65f836ab9191dab238586c836833eba05b612
pacfrog <justin.sw.yang@gmail.com>
2025-12-11 18:05:20 +0800

    Add mkdir command to make sure the directory existed

--- misc/auto-neon.sh
+++ misc/auto-neon.sh
@@ -10,31 +10,12 @@ export DBUS_SESSION_BUS_ADDRESS="${DBUS_SESSION_BUS_ADDRESS:-unix:path=/run/user
 [ -S "$XDG_RUNTIME_DIR/bus" ] || exit 0
 
 # Neon-wallpapers
-WALLPAPERS=(
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon10.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon11.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon12.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon13.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon14.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon15.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon16.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon17.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon18.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon19.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon1.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon20.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon2.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon3.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon4.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon5.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon6.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon7.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon8.jpg"
-	"$HOME/.local/share/backgrounds/2025-12-09-11-31-55-neon9.jpg"
-)
+WALLPAPERS=( "$HOME/.local/share/backgrounds/"*neon*.jpg )
 
 # Read/Write the value in wall_index
 STATE="$HOME/.local/state/auto-neon/wall_index"
+mkdir -p "$(dirname "$STATE")"
+
 [[ -f "$STATE" ]] && idx=$(<"$STATE") || idx=0
 (( idx = (idx + 1) % ${#WALLPAPERS[@]} ))
 echo "$idx" > "$STATE"
